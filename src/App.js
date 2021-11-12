05fee9c04457937cd2342807d7c6d03ff3fcff60
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-11-12 15:16:36 +0600

    changed tailwind config to implement themes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="py-6 px-4 rounded-lg bg-green-100 border border-green-300 text-lg text-gray-800 font-semibold tracking-wide">
-        ✅ <span className="text-green-600">TailwindCSS</span> is working!
-      </div>
-    </div>
+    
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          numpad: 'var(--color-text-numpad)',
+          command: 'var(--color-text-command)',
+          base: 'var(--color-text-base)',
+          eq: 'var(--color-text-eq)', 
+        }
+      },
+      backgroundColor: {
+        skin: {
+          fill: 'var(--color-fill)',
+          display: 'var(--color-display)',
+          body: 'var(--color-body)',
+          numpad: 'var(--color-button-numpad)',
+          command: 'var(--color-button-command)',
+          accent: 'var(--color-accent)',
+          'hover-accent': 'var(--color-accent-hover)',
+          'hover-numpad': 'var(--color-button-numpad-hover)',
+          'hover-command': 'var(--color-button-command-hover)',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
